602f28282e424f9482e7832e815483c08206925d
ZhangPluto <lionelzhang99@gmail.com>
2021-04-19 08:36:04 -0400

    Add test code, Create main page

--- src/view/mainPage.java
+++ src/view/mainPage.java
@@ -1,11 +1,25 @@
 package view;
 
+import javax.swing.*;
+
 /**
  * @ProjectName Kakuro
  * @ClassName mainPage
  * @Author renhaozhang
- * @Date 2021-03-28 9:19 p.m.
+ * @Date 2021-04-19 8:28 a.m.
  * @PackageName view
  **/
 public class mainPage {
+    private JPanel mainPanel;
+    private JButton buttonLeaderBoard;
+    private JButton buttonIntroduction;
+    private JButton buttonBegin;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("mainPage");
+        frame.setContentPane(new mainPage().mainPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
